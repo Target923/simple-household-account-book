fce2bd29225082168b4bb9fe3da5275741ee8f5e
Target <TargetCreate923@gmail.com>
2025-08-08 12:01:23 +0900

    add draw UI & JsDoc comment

--- app/components/CategoryForm.js
+++ app/components/CategoryForm.js
@@ -1,19 +1,38 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import styles from './CategoryForm.module.css'
 
 export default function CategoryForm() {
     const [categoryName, setCategoryName] = useState('');
-    const [categories, setCategories] = useState([
-        { id: '1', name: '食費' },
-        { id: '2', name: '交通費' }
-    ]);
+    const [categories, setCategories] = useState([])
 
+    useEffect(() => {
+        const storedCategories = JSON.parse(localStorage.getItem('categories')) || [];
+        if (storedCategories.length > 0) {
+            setCategories(storedCategories);
+        } else {
+            const initialCategories = [
+                { id: Date.now().toString(), name: '食費' },
+                { id: (Date.now() + 1).toString(), name: '交通費' },
+            ];
+            setCategories(initialCategories);
+            localStorage.setItem('categories', JSON.stringify(initialCategories))
+        }
+    }, []);
+
+    /**
+     * 入力フォームの値が変更されるたびに実行、categoryNameの状態を更新
+     * @param {ChangeEvent<HTMLInputElement>} event - Input要素の変更イベント
+     */
     function handleChange(event) {
         setCategoryName(event.target.value);
     }
 
+    /**
+     * 登録ボタンクリック時に実行、新規カテゴリを保存
+     * @param {MouseEvent<HTMLButtonElement>} event 
+     */
     function handleClick(event) {
         event.preventDefault();
         if (categoryName) {
@@ -21,6 +40,10 @@ export default function CategoryForm() {
         }
     }
 
+    /**
+     * 新しいカテゴリを生成し、ローカルストレージとstateの両方を更新
+     * state更新時、UIが自動的に再描画
+     */
     function saveCategoryInLocalStorage() {
         const newCategory = {
             id: Date.now().toString(),
@@ -28,9 +51,7 @@ export default function CategoryForm() {
         };
 
         const existingCategories = JSON.parse(localStorage.getItem('categories')) || [];
-        
         const updateCategories = [...existingCategories, newCategory];
-
         setCategories(updateCategories);
 
         localStorage.setItem('categories', JSON.stringify(updateCategories));
@@ -45,8 +66,14 @@ export default function CategoryForm() {
                 value={categoryName}
                 onChange={handleChange}
             ></input>
-
             <button onClick={handleClick}>この名前で登録する</button>
+
+            <h2>カテゴリ一覧</h2>
+            <ul>
+                {categories.map((category) => (
+                    <li key={category.id}>{category.name}</li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
