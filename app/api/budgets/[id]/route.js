3bbce8f9d0a6d5f965e9d175b396f5529e5ad4ec
Target <TargetCreate923@gmail.com>
2025-09-17 14:28:26 +0900

    fixed import authOption directory path

--- app/api/budgets/[id]/route.js
+++ app/api/budgets/[id]/route.js
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma"
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function PUT(request, { params }) {
     const session = await getServerSession(authOptions);
